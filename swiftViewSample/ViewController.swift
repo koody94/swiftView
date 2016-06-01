1af0edd6798bacf21099e4522273da555707e3c2
doyeon koo <koody94@naver.com>
2016-06-01 09:32:46 +0900

    Initial Commit

---
+++ swiftViewSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftViewSample
+//
+//  Created by MF839-013 on 2016. 6. 1..
+//  Copyright © 2016년 SDS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
